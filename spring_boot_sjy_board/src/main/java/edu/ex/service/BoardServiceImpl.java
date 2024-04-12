e75ec20f13efeafa1a9639f72285a315bff82579
welcomeglory <godbu2021@naver.com>
2024-04-12 15:49:55 +0900

    impl add

---
+++ spring_boot_sjy_board/src/main/java/edu/ex/service/BoardServiceImpl.java
@@ -0,0 +1,27 @@
+package edu.ex.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.ex.mapper.BoardMapper;
+import edu.ex.vo.BoardVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	@Override
+	public List<BoardVO> getList(){
+		log.info("getList..");
+		
+		return boardMapper.getList();
+	}
+	
+}
+
