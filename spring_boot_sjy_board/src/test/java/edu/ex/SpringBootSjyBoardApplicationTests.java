c351e263894fd08eeea8562ae089d7bab3555c08
welcomeglory <godbu2021@naver.com>
2024-04-12 12:06:01 +0900

    init

---
+++ spring_boot_sjy_board/src/test/java/edu/ex/SpringBootSjyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSjyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
