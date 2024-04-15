f045aa8105f27be5acb19ec0f71e7f7446b04771
welcomeglory <godbu2021@naver.com>
2024-04-15 12:40:50 +0900

    reply_view impl

--- spring_boot_sjy_board/src/main/java/edu/ex/service/BoardServiceImpl.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/service/BoardServiceImpl.java
@@ -45,6 +45,12 @@ public class BoardServiceImpl implements BoardService{
 
 		return boardMapper.updateBoard(boardVO);
 	}
+	@Override
+	public void writeReply(BoardVO boardVO) {
+		log.info("writeReply..");
+
+		boardMapper.insertReply(boardVO);
+	}
 
 }
 
