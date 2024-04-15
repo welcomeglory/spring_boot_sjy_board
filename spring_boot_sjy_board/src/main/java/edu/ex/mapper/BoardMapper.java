f1bf9e50dfd91e2b66185472705dace935e0ec75
welcomeglory <godbu2021@naver.com>
2024-04-15 15:16:07 +0900

    hit mapper

--- spring_boot_sjy_board/src/main/java/edu/ex/mapper/BoardMapper.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/mapper/BoardMapper.java
@@ -9,12 +9,13 @@ import edu.ex.vo.BoardVO;
 @Mapper
 public interface BoardMapper {
 	List<BoardVO> getList();
+	void uphitBoard(int bid);
 	BoardVO read(int bid);
 	int delete(int bid);
 	int insertBoard(BoardVO boardVO);
 	int updateBoard(BoardVO boardVO);
 	//댓글관련
-	void updateShape(BoardVO boardVO); //답글위치
+	void updateShape(BoardVO boardVO); //답글위치(들어갈자리에 스텝을 한칸씩 밀어내는 함수)
 	void insertReply(BoardVO boardVO);//답글등록
 }
 
