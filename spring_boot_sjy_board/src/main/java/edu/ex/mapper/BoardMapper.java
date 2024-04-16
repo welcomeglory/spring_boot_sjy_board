4b8ee0d8409aa5b8d2c1265fa8c1a1a53263d4de
welcomeglory <godbu2021@naver.com>
2024-04-16 16:06:34 +0900

    paging add mapper

--- spring_boot_sjy_board/src/main/java/edu/ex/mapper/BoardMapper.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/mapper/BoardMapper.java
@@ -15,9 +15,11 @@ public interface BoardMapper {
 	int delete(int bid);
 	int insertBoard(BoardVO boardVO);
 	int updateBoard(BoardVO boardVO);
+	
 	//댓글관련
 	void updateShape(BoardVO boardVO); //답글위치(들어갈자리에 스텝을 한칸씩 밀어내는 함수)
 	void insertReply(BoardVO boardVO);//답글등록
+	
 	//페이징관련
 	List<BoardVO> getListWithPaging(Criteria cri);
 	int getTotalCount();
