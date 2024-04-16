0254b4943ae46badc07fd2779e40da0a9843af61
welcomeglory <godbu2021@naver.com>
2024-04-16 16:06:49 +0900

    paging add impl

--- spring_boot_sjy_board/src/main/java/edu/ex/service/BoardServiceImpl.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/service/BoardServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import edu.ex.mapper.BoardMapper;
+import edu.ex.page.Criteria;
 import edu.ex.vo.BoardVO;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,34 +23,49 @@ public class BoardServiceImpl implements BoardService{
 		
 		return boardMapper.getList();
 	}
+	
 	@Override
 	public BoardVO get(int bid) {
 		log.info("read..");
 		boardMapper.uphitBoard(bid);		
 		return boardMapper.read(bid);
 	}
+	
 	@Override
 	public int remove(int bid) {
 		log.info("delete..");
-
 		return boardMapper.delete(bid);
 	}
+	
 	@Override
 	public void writeBoard(BoardVO boardVO) {
 		log.info("insertBoard..");
 		boardMapper.insertBoard(boardVO);		
 	}
+	
 	@Override
 	public int modifyBoard(BoardVO boardVO) {
 		log.info("modifyBoard..");
-
 		return boardMapper.updateBoard(boardVO);
 	}
+	
 	@Override
 	public void writeReply(BoardVO boardVO) {
-		log.info("writeReply..");
+		log.info("writeReply()..");
 		boardMapper.updateShape(boardVO);//답글의 위치를 먼저 잡아준 후
 		boardMapper.insertReply(boardVO);//답글을 등록한다.
+	}
+	
+	@Override
+	public int getTotal() {
+		log.info("getTotal()..");
+		return boardMapper.getTotalCount();
+	}
+	
+	@Override
+	public List<BoardVO> getListWithPaging(Criteria cri) {
+		log.info("getListWithPaging()..");
+		return boardMapper.getListWithPaging(cri);
 	}
 
 }
