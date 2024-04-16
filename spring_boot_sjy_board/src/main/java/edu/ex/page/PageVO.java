3c8de94775df1786989785979c058b4d60b6bd50
welcomeglory <godbu2021@naver.com>
2024-04-16 11:47:04 +0900

    paging vo

---
+++ spring_boot_sjy_board/src/main/java/edu/ex/page/PageVO.java
@@ -0,0 +1,18 @@
+package edu.ex.page;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class PageVO {
+	private int startPage;//화면에 보여지는 페이지 시작번호
+	private int endPage;//화면에 보여지는 끝번호
+	
+	private boolean pre, next; //이전과 다음으로 이동가능한 링크 표시
+	private int total;//전체 게시판 글 수
+	
+	
+}
