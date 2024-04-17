8e5afc3d9bd394ef250b72c0c1f99652d621458d
welcomeglory <godbu2021@naver.com>
2024-04-17 11:39:13 +0900

    ver2 paging servicce

--- spring_boot_sjy_board/src/main/java/edu/ex/two/BService2.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/two/BService2.java
@@ -7,6 +7,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.ex.page.Criteria;
 import edu.ex.vo.BoardVO;
 
 @Service
@@ -17,5 +18,13 @@ public class BService2 {
 	public List<BoardVO> selectBoardList() throws Exception{
 		//모든 CRUD에 대해 함수를 제공해줌.
 		return SqlSession.selectList("board.selectBoardList");		
-	}	
+	}					
+	public List<BoardVO> selectBoardPagingList(Criteria cri) throws Exception{
+		//모든 CRUD에 대해 함수를 제공해줌.
+		return SqlSession.selectList("board.selectBoardPagingList",cri);		
+	}
+	//하나 뽑아낼땐 selectOne()사용
+	public int getTotal() throws Exception{
+		return  SqlSession.selectOne("board.getTotal");
+	}
 }
