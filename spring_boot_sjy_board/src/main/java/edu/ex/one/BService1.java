c7f67ed1def52d471dfa298ae5de40fa366832b8
welcomeglory <godbu2021@naver.com>
2024-04-17 10:08:08 +0900

    SqlSessionTemplate add

--- spring_boot_sjy_board/src/main/java/edu/ex/one/BService1.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/one/BService1.java
@@ -3,6 +3,7 @@ package edu.ex.one;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,9 @@ import edu.ex.vo.BoardVO;
 @Service
 public class BService1 {
 	@Autowired
-	private SqlSession SqlSession;
+//	private SqlSession SqlSession; //마이바티스
+	private SqlSessionTemplate SqlSession;//마이바티스
+
 	
 	public List<BoardVO> selectBoardList() throws Exception{
 		IBDao dao = SqlSession.getMapper(IBDao.class);
