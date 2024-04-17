831dbebee7189b4a70926ab51b4e677f59bd674d
welcomeglory <godbu2021@naver.com>
2024-04-17 09:51:04 +0900

    IBDao service

---
+++ spring_boot_sjy_board/src/main/java/edu/ex/one/BService1.java
@@ -0,0 +1,22 @@
+package edu.ex.one;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.ex.vo.BoardVO;
+
+@Service
+public class BService1 {
+	@Autowired
+	private SqlSession SqlSession;
+	
+	public List<BoardVO> selectBoardList() throws Exception{
+		IBDao dao = SqlSession.getMapper(IBDao.class);
+		return dao.listdao();
+	}
+	
+	
+}
