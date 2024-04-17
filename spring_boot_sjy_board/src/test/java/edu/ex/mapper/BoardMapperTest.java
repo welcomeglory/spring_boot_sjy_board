71d0d1770cf8e2fc606b3e799c710cd03ba6c2f4
welcomeglory <godbu2021@naver.com>
2024-04-17 11:47:24 +0900

    ver3 test

--- spring_boot_sjy_board/src/test/java/edu/ex/mapper/BoardMapperTest.java
+++ spring_boot_sjy_board/src/test/java/edu/ex/mapper/BoardMapperTest.java
@@ -2,6 +2,7 @@ package edu.ex.mapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,23 +16,30 @@ class BoardMapperTest {
 	
 	@Autowired
 	private BoardMapper boardMapper;
-
+	
+	@Disabled
 	@Test
 	void testBoardMapper() {
 		assertNotNull(boardMapper);
 	}
-	
+	@Disabled
 	@Test
 	void testSelectList() {
 		for(BoardVO boardVO : boardMapper.getList() ) {
 			log.info("연결"+boardVO);
-
 		}
 	}
-	
+	@Disabled
 	@Test
 	void testRead() {
 		System.out.println(boardMapper.read(26));
+	}
+	
+	@Test
+	void testSelectList2() {
+		for(BoardVO boardVO : boardMapper.getList2() ) {
+			log.info("연결"+boardVO);
+		}
 	}
 
 }
