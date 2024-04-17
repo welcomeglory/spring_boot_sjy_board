4526b94c33ab91a38ca720828dae908b5d5038f0
welcomeglory <godbu2021@naver.com>
2024-04-17 20:23:18 +0900

    co join mapper

--- spring_boot_sjy_board/src/main/java/edu/ex/mapper/CompanyMapper.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/mapper/CompanyMapper.java
@@ -2,9 +2,14 @@ package edu.ex.mapper;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+
+import edu.ex.vo.DeptEmpVO;
 import edu.ex.vo.EmpDeptVO;
+import edu.ex.vo.SalgradeEmpVO;
 
 @Mapper
 public interface CompanyMapper {
 	List<EmpDeptVO> getEmpDept();	
+	List<DeptEmpVO> getDeptEmpList();
+	List<SalgradeEmpVO> getSalgradeEmpList();
 }
