2658cd6878fbd14bd4bc190bffe993eb8dc3e1ca
welcomeglory <godbu2021@naver.com>
2024-04-18 17:44:00 +0900

    user controller

---
+++ spring_boot_sjy_board/src/main/java/edu/ex/controller/UserController.java
@@ -0,0 +1,39 @@
+package edu.ex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.ex.service.UserService;
+import edu.ex.vo.UserVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class UserController {
+	@Autowired 
+	UserService userService;
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		System.out.println("index()..");
+//		model.addAttribute("DeptEmps",companyService.getDeptEmpList());	
+		return "index";
+	}	
+	@GetMapping("/login")
+	public String login() {
+		System.out.println("login()..");
+		return "login";
+	}
+	@PostMapping("/write")
+	public String write(UserVO user,Model model) {
+		System.out.println("write()..");
+		userService.insertUser(user);	
+		userService.insertAuthoruties(user);	
+		return "redirect:/index";
+	}	
+}
+
+
