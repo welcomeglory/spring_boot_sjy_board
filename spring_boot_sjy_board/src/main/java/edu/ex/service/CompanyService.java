a2f16a08c8a923185f91ec669b328b2c7a51bea6
welcomeglory <godbu2021@naver.com>
2024-04-18 11:04:56 +0900

    salgrade join  service

--- spring_boot_sjy_board/src/main/java/edu/ex/service/CompanyService.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/service/CompanyService.java
@@ -3,11 +3,9 @@ package edu.ex.service;
 import java.util.List;
 
 import edu.ex.vo.DeptEmpVO;
-import edu.ex.vo.SalgradeEmpVO;
 
 public interface CompanyService {
 
-	List<DeptEmpVO> getDeptEmpList();
-	List<SalgradeEmpVO> getSalgradeEmpList();
+	List<DeptEmpVO> getDeptEmpList(); //일대다
 	
 }
