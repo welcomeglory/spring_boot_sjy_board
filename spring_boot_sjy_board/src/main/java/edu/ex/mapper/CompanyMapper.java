75b27f570d7ce3befd2c8c5ff43d646bd65e9258
welcomeglory <godbu2021@naver.com>
2024-04-18 11:04:29 +0900

    salgrade join mapper

--- spring_boot_sjy_board/src/main/java/edu/ex/mapper/CompanyMapper.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/mapper/CompanyMapper.java
@@ -5,11 +5,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 import edu.ex.vo.DeptEmpVO;
 import edu.ex.vo.EmpDeptVO;
-import edu.ex.vo.SalgradeEmpVO;
 
 @Mapper
 public interface CompanyMapper {
 	List<EmpDeptVO> getEmpDept();	
 	List<DeptEmpVO> getDeptEmpList();
-	List<SalgradeEmpVO> getSalgradeEmpList();
 }
