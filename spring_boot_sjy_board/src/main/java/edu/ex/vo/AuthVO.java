6b95c43a22568b83891471fa5505a8f44ae21d13
welcomeglory <godbu2021@naver.com>
2024-04-18 17:45:02 +0900

    auth vo

---
+++ spring_boot_sjy_board/src/main/java/edu/ex/vo/AuthVO.java
@@ -0,0 +1,20 @@
+package edu.ex.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//USERNAME  NOT NULL VARCHAR2(50) 
+//AUTHORITY NOT NULL VARCHAR2(50) 
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AuthVO {
+	private String username;
+	private String authority;
+}
