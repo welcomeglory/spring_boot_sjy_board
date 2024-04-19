b54acee4f96b7c16ea5c3d474e6dc8367c700200
welcomeglory <godbu2021@naver.com>
2024-04-19 09:43:13 +0900

    VER3 mapper

--- spring_boot_sjy_board/src/main/java/edu/ex/mapper/UserMapper.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package edu.ex.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import edu.ex.vo.UserVO;
 
@@ -8,7 +9,14 @@ public interface UserMapper {
 	
 	public UserVO getUser(String username);
 	
+//	public int insertUser(UserVO userVO);
+//	
+//	public void insertAuthoruties(UserVO userVO);
+		
+	@Insert("insert into users(username,password) VALUES (#{username},#{password})")
 	public int insertUser(UserVO userVO);
+	
+	@Insert("insert into authorities(username,authority) values(#{username},'ROLE_USER')")
 	public void insertAuthoruties(UserVO userVO);
 	
 }
