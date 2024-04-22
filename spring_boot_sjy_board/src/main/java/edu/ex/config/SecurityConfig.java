443c9c281373147df1cf562a70deeeeeedb7a453
welcomeglory <godbu2021@naver.com>
2024-04-22 16:02:36 +0900

    static resources

--- spring_boot_sjy_board/src/main/java/edu/ex/config/SecurityConfig.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/config/SecurityConfig.java
@@ -10,9 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	//정적 리소스를 스프링 시큐리티에 적용되지 않도록 아래와 같이 설정한다.
+	//이제 더이상 리소트파일들은 스프링 시큐리티에서 관리하지 않는다.
    @Override
    public void configure(WebSecurity web) throws Exception {
-      //web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	   //web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
       web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
    }
 
