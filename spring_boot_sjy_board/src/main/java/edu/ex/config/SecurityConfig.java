95edb2b38ff673755480a5bd8d7a1bf9050eb42a
welcomeglory <godbu2021@naver.com>
2024-04-22 16:57:26 +0900

    auth

--- spring_boot_sjy_board/src/main/java/edu/ex/config/SecurityConfig.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	   //web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
       web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
    }
+   
+   //인증과 권한 설정
+   @Override
+   public void configure(HttpSecurity http) throws Exception {
+	   //우선 CSRF설정을 해제한다.
+	   //초기 개발시만 해주는게 좋다.
+	   http.csrf().disable();
+	   
+	   //권한설정
+	   http.authorizeRequests()
+	   .antMatchers("/user/**").hasAnyRole("USER")
+	   .antMatchers("/admin/**").hasAnyRole("ADMIN")
+	   .antMatchers("/**").permitAll();
+	   
+	   http.formLogin();//스프링 시큐리티에 있는 기본 로그인 폼을 사용하겠다.
+   }
+   
    //테스트용 유저 만들기(인메모리 방식)
    @Override
    public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -26,8 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	   auth.inMemoryAuthentication()
 	   .withUser("member").password("{noop}member").roles("USER")
 	   .and()
-	   .withUser("admin").password("{noop}admin").roles("ADMIN");
-	 
+	   .withUser("admin").password("{noop}admin").roles("ADMIN");	 
    }
 
 }
