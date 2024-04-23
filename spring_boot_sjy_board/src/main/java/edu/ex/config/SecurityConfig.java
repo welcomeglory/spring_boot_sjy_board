93fa1fa19dfd2452e4dc855550cbf74cd02055b3
welcomeglory <godbu2021@naver.com>
2024-04-23 17:43:56 +0900

    SecurityConfig

--- spring_boot_sjy_board/src/main/java/edu/ex/config/SecurityConfig.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/config/SecurityConfig.java
@@ -1,55 +1,65 @@
 package edu.ex.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import edu.ex.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	//정적 리소스를 스프링 시큐리티에 적용되지 않도록 아래와 같이 설정한다.
-	//이제 더이상 리소트파일들은 스프링 시큐리티에서 관리하지 않는다.
-   @Override
-   public void configure(WebSecurity web) throws Exception {
-	   //web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-      web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
-   }
-   
-   //인증과 권한 설정
-   @Override
-   public void configure(HttpSecurity http) throws Exception {
-	   //우선 CSRF설정을 해제한다.
-	   //초기 개발시만 해주는게 좋다.
-	   http.csrf().disable();
-	   
-	   //권한설정
-	   http.authorizeRequests()
-	   .antMatchers("/user/**").hasAnyRole("ROLE_USER")
-	   .antMatchers("/admin/**").hasAnyRole("ROLE_ADMIN")
-	   .antMatchers("/**").permitAll();
-	   	   
-	   http.formLogin()
-	   .loginPage("/login")//logPage()는 말그대로 로그인할 페이지 url이고
-	   .usernameParameter("id")// <input type="text" id="id" name="id" />
-	   .passwordParameter("pw")// <input type="password" id="password" name="pw"/>
-	   .defaultSuccessUrl("/")//login성공페이지
-	   .permitAll();//모든 유저가 로그인 화면은 볼 수 있게 한다.	   
-   }
-   
-   //테스트용 유저 만들기(인메모리 방식)
-   @Override
-   public void configure(AuthenticationManagerBuilder auth) throws Exception {
-	   //{noop} 암호화 안된 password를 의미함.
-	   auth.inMemoryAuthentication()
-	   .withUser("member").password("{noop}member").roles("USER")
-	   .and()
-	   .withUser("admin").password("{noop}admin").roles("ADMIN");	 
-   }
+	 @Autowired
+	 private CustomUserDetailsService customUserDetailsService;
+	
+/*	이미지(가영이)가 보이지 않는다.
+	우선 정적파일들은 시큐리티에 적용되지 않도록 아래와 같이 설정을 한다.
+	이제 더이상 리소스파일들은 스프링 시큐리티에서 관리를 하지 않는다.	*/
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		//우선 CSRF설정을 해제한다.
+		//초기 개발시만 해주는게 좋다.
+		http.csrf().disable();
+		
+		/*권한 설정*/
+		http.authorizeRequests()
+		.antMatchers("/user/**").hasAnyRole("USER") 
+		.antMatchers("/admin/**").hasAnyRole("ADMIN")
+		.antMatchers("/**").permitAll();
+		
+		http.formLogin()
+		        .loginPage("/login") //loginPage() 는 말그대로 로그인할 페이지 url 이고
+		        .usernameParameter("id")
+		        .passwordParameter("pw")
+		        .defaultSuccessUrl("/")
+		        .permitAll(); //모든 유저가 로그인 화면은 볼 수 있게 한다
+	}
+	
+	//테스트용 유저 만들기(인메모리 방식)
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		/*
+		 * auth .inMemoryAuthentication()
+		 * .withUser("member").password("{noop}member").roles("USER") .and()
+		 * .withUser("admin").password("{noop}admin").roles("ADMIN");
+		 */
+		auth.userDetailsService(customUserDetailsService)
+				.passwordEncoder(new BCryptPasswordEncoder());
+		
+	}
 
 }
