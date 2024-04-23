7f699a9698797e13914a67a4d3334105f85f1343
welcomeglory <godbu2021@naver.com>
2024-04-23 17:44:22 +0900

    home con

--- spring_boot_sjy_board/src/main/java/edu/ex/controller/HomeController.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/controller/HomeController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import edu.ex.service.EmpService;
 import edu.ex.service.UserService;
 import edu.ex.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
@@ -13,50 +16,39 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class HomeController {
-	@Autowired 
-	private UserService userService;
+	
+	@Autowired
+	private EmpService empService;	
 	
 	@GetMapping("/")
 	public String home(Model model) {
 		log.info("home()..");
+		
 		return "home";
-	}	
-//	@GetMapping("/index")
-//	public void eshopper() {
-//		log.info("eshopper()..");
-//	}	
+	}
 	
 	
-	 @GetMapping("/user/userHome") 
-	 public void userHome() {
-		 log.info("userHome().."); 
-	 }
-	 
-	
-//	@GetMapping("/user/userHome")
-//	public String userHome() {
-//		System.out.println("userHome()..");
-//		return "/user/userHome";
-//	}	
+	@GetMapping("/user/userHome")
+	public void userHome() {
+		log.info("userHome()..");	
+	}
 	
 	@GetMapping("/admin/adminHome")
 	public void adminHome() {
 		log.info("adminHome()..");
-	}	
+	}
+
 	
-//	@GetMapping("/login")
-//	public String login() {
-//		System.out.println("login()..");
-//		return "login";
-//	}
-	@PostMapping("/write")
-	public String write(UserVO userVO,Model model) {
-		log.info("write()..");
-//		userService.insertUser(userVO);	
-//		userService.insertAuthoruties(userVO);	
-		userService.addUser(userVO);
-		return "redirect:/index";
-	}	
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		log.info("index()..");
+		
+		model.addAttribute("deptEmps",empService.getDeptEmpList());	
+		
+		return "index";
+	}
+	
 }
 
 
