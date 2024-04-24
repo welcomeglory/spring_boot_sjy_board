ba3a6bb937da8d973e3432386de6b39fcf9ede9e
welcomeglory <godbu2021@naver.com>
2024-04-24 15:39:38 +0900

    direct mapping

--- spring_boot_sjy_board/src/main/java/edu/ex/mapper/CompanyMapper.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/mapper/CompanyMapper.java
@@ -2,12 +2,17 @@ package edu.ex.mapper;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import edu.ex.vo.DeptEmpVO;
 import edu.ex.vo.EmpDeptVO;
+import edu.ex.vo.EmpVO;
 
 @Mapper
 public interface CompanyMapper {
 	List<EmpDeptVO> getEmpDept();	
-	List<DeptEmpVO> getDeptEmpList();
+	List<DeptEmpVO> getDeptEmpList();//일대다
+	
+	@Select("select * from emp where ename = #{ename}")
+	EmpVO getEmp(String ename);
 }
