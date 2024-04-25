3bd5b90e86c0be1e171a873980cd8492bdaa37b5
welcomeglory <godbu2021@naver.com>
2024-04-25 10:04:33 +0900

    edit

--- spring_boot_sjy_board/src/test/java/edu/ex/mapper/UserMapperTest.java
+++ spring_boot_sjy_board/src/test/java/edu/ex/mapper/UserMapperTest.java
@@ -2,14 +2,18 @@ package edu.ex.mapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import edu.ex.vo.BoardVO;
+import edu.ex.vo.EmpVO;
 import edu.ex.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
+
 
 @Slf4j
 @SpringBootTest
@@ -21,11 +25,15 @@ class UserMapperTest {
    @Autowired
    private PasswordEncoder passwordEncoder;
    
+   @Autowired
+	private CompanyMapper companyMapper;
+   
+   @Disabled
    @Test
    void testUserMapper() {
       assertNotNull(userMapper);
    }
-   
+   @Disabled
    @Test
    void testGetUser() {
       UserVO user = userMapper.getUser("admin");
@@ -33,26 +41,26 @@ class UserMapperTest {
       
       System.out.println(user);
    }
+   
+  @Disabled
    @Test
    void testInsertUser() {
-//      UserVO user = new UserVO();
-//      user.setUsername("admin3");
-//      user.setPassword(new BCryptPasswordEncoder().encode("admin3"));
-//      user.setEnabled("1");
-//      
-//      userMapper.insertUser(user);
-//      userMapper.insertAuthoruties(user);    
-
       UserVO user = new UserVO();
-      user.setUsername("user2");
-      user.setPassword(passwordEncoder.encode("user2"));
+      user.setUsername("admin3");
+      user.setPassword(new BCryptPasswordEncoder().encode("admin3"));
       user.setEnabled("1");
       
       userMapper.insertUser(user);
-      userMapper.insertAuthoruties(user);    
+      userMapper.insertAuthoruties(user);  	  
       
    }
-   
-   
 
+  @Test
+   void testMatcher() {
+      UserVO user = userMapper.getUser("admin2");
+      boolean isMatch = passwordEncoder.matches("admin2", user.getPassword());
+      assertEquals(isMatch, true);
+	      
+   }
+  
 }
