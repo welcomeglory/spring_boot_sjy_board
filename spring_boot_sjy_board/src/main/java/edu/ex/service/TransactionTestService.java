c60942f94cc9eb816f6bda4794837772a1df72d7
welcomeglory <godbu2021@naver.com>
2024-04-25 12:36:29 +0900

    TransactionTest1

---
+++ spring_boot_sjy_board/src/main/java/edu/ex/service/TransactionTestService.java
@@ -0,0 +1,37 @@
+package edu.ex.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.ex.mapper.BoardMapper;
+import edu.ex.vo.BoardVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class TransactionTestService {
+	
+	@Autowired
+	private  BoardMapper mapper;
+	
+	public void TransactionTest1() {
+		
+		log.info("TransactionTest1()..");
+		
+		 BoardVO boardVO = new BoardVO();
+		 boardVO.setBcontent("트랜잭션1");
+		 boardVO.setBname("트랜잭션1");
+		 boardVO.setBtitle("트랜잭션1");
+		
+		 mapper.insertBoard(boardVO);
+		 
+		 BoardVO boardVO2 = new BoardVO();
+		 boardVO.setBcontent("트랜잭션2");
+		 boardVO.setBname("트랜잭션2");
+		 boardVO.setBtitle("트랜잭션2");
+		
+		 mapper.insertBoard(boardVO2);
+
+	}
+}
+
