80dc283b909abaae8714908659adf6545b9d6e79
welcomeglory <godbu2021@naver.com>
2024-04-25 15:21:28 +0900

    rollback test

--- spring_boot_sjy_board/src/main/java/edu/ex/service/TransactionTestService.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/service/TransactionTestService.java
@@ -32,7 +32,67 @@ public class TransactionTestService {
 		 boardVO2.setBname("트랜잭션2");
 		 boardVO2.setBtitle("트랜잭션2");
 		
-		 mapper.insertBoard(boardVO2);
+		 mapper.insertBoard(boardVO2); 
+
+	}
+	
+	public void TransactionTest2() {
+		
+		log.info("TransactionTest2()..");
+		
+		 BoardVO boardVO = new BoardVO();
+		 boardVO.setBcontent("트랜잭션1");
+		 boardVO.setBname("트랜잭션1");
+		 boardVO.setBtitle("트랜잭션1");
+		
+		 mapper.insertBoard(boardVO);
+		 
+		 BoardVO boardVO2 = new BoardVO();
+		 boardVO2.setBcontent("트랜잭션2");
+		 boardVO2.setBname("트랜잭션2");
+		 boardVO2.setBtitle("트랜잭션2");
+		 
+		 //일부러 트랜잭션을 위한 테스트로 null 값 셋팅
+		 boardVO2 = null;		
+		 mapper.insertBoard(boardVO2);	 
+
+	}
+	
+	@Transactional //롤백
+	public void TransactionTest3() {
+		
+		log.info("TransactionTest3()..");
+		
+		 BoardVO boardVO = new BoardVO();
+		 boardVO.setBcontent("트랜잭션3");
+		 boardVO.setBname("트랜잭션3");
+		 boardVO.setBtitle("트랜잭션3");
+		
+		 mapper.insertBoard(boardVO);
+		 
+		 BoardVO boardVO2 = new BoardVO();
+		 boardVO2.setBcontent("트랜잭션3");
+		 boardVO2.setBname("트랜잭션3");
+		 boardVO2.setBtitle("트랜잭션3");
+		 
+		 boardVO2 = null;		
+		 mapper.insertBoard(boardVO2);	 
+
+	}
+	
+	@Transactional //롤백
+	public void TransactionTest4() {
+		
+		log.info("TransactionTest4()..");
+		
+		 BoardVO boardVO = new BoardVO();
+		 boardVO.setBcontent("트랜잭션4");
+		 boardVO.setBname("트랜잭션4");
+		 boardVO.setBtitle("트랜잭션4");
+		
+		 mapper.insertBoard(boardVO);
+		 
+		 throw new RuntimeException("RuntimeException for rollback");
 
 	}
 }
