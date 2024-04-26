50265ee5ccfe13d69a35718116a2cdfd45bb5656
welcomeglory <godbu2021@naver.com>
2024-04-26 10:42:13 +0900

    edit

--- spring_boot_sjy_board/src/main/java/edu/ex/controller/RestBoardController.java
+++ spring_boot_sjy_board/src/main/java/edu/ex/controller/RestBoardController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -122,5 +123,9 @@ public class RestBoardController {
 		mv.setViewName("/board/rest_list");
 		return mv;
 	}
+	
+	
+	
+	
 
 }
