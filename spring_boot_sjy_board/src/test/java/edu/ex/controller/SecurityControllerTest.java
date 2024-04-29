8094344042ddd88d8750fc2edbb2534d6bae6fee
welcomeglory <godbu2021@naver.com>
2024-04-29 10:04:15 +0900

    admin page test

---
+++ spring_boot_sjy_board/src/test/java/edu/ex/controller/SecurityControllerTest.java
@@ -0,0 +1,40 @@
+package edu.ex.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.MultiValueMap;
+
+import edu.ex.page.Criteria;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+class SecurityControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;//웹브라우져 가져오기
+	
+	@Disabled
+	@Test
+	@WithMockUser(username="admin", password="admin", authorities="ROLE_ADMIN")
+	void testAdminPage() throws Exception {
+		
+		//관리자 홈
+		mockMvc.perform(MockMvcRequestBuilders.get("/admin/adminHome"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andDo(print());	
+		
+	}
+		
+}
